79ab0fe45762d9acb5cc4a5f0bcecade5811a318
Drallireeh <kenny.herillard@hotmail.fr>
2026-07-18 13:46:11 +0200

    Questions for part 1 completed

--- data.js
+++ data.js
@@ -7,8 +7,15 @@ const QUIZ_DATA = {
 
   /* ---------- MANCHE 1 : Questions (QCM ou +/-) ---------- */
   round1: {
-    timerSeconds: 20, // durée par défaut du chrono (en secondes)
+    timerSeconds: 12, // durée par défaut du chrono (en secondes)
     questions: [
+      {
+        question: "Quel est le plus grand os du corps humain  ?",
+        type: "qcm", // "qcm" (4 réponses A-D) ou "plusminus" (+/-)
+        answers: ["Tibia", "Humérus", "Fémur", "Fibula"],
+        correctIndex: 2
+        // timer: 15, // (optionnel) surcharge la durée par défaut pour cette question
+      },
       {
         question: "Quelle est la capitale de l'Australie ?",
         type: "qcm", // "qcm" (4 réponses A-D) ou "plusminus" (+/-)
@@ -17,21 +24,9 @@ const QUIZ_DATA = {
         // timer: 15, // (optionnel) surcharge la durée par défaut pour cette question
       },
       {
-        question: "Le Mont Blanc culmine à plus de 5000 mètres.",
-        type: "plusminus",
-        answers: ["Vrai (+)", "Faux (−)"],
-        correctIndex: 1
-      },
-      {
         question: "Qui a peint 'La Nuit étoilée' ?",
         type: "qcm",
         answers: ["Claude Monet", "Vincent van Gogh", "Pablo Picasso", "Edgar Degas"],
-        correctIndex: 1
-      },
-      {
-        question: "Le Wi-Fi utilise exclusivement la fréquence 2.4 GHz.",
-        type: "plusminus",
-        answers: ["Vrai (+)", "Faux (−)"],
         correctIndex: 1
       },
       {
@@ -41,23 +36,102 @@ const QUIZ_DATA = {
         correctIndex: 2
       },
       {
-        question: "Quel est le plus long fleuve du monde ?",
+        question: "Quel pays possède le plus grand nombre d'îles ?",
         type: "qcm",
-        answers: ["Le Nil", "L'Amazone", "Le Mississippi", "Le Yangtsé"],
+        answers: ["Indonésie", "Japon", "Suède", "Philippines"],
+        correctIndex: 2
+      },
+      {
+        question: "Madonna a-t-elle vendu plus ou moins d'albums que Céline Dion ?",
+        type: "plusminus",
+        answers: ["+", "-"],
         correctIndex: 0
       },
       {
-        question: "Une équipe de rugby à XV compte 15 joueurs sur le terrain.",
-        type: "plusminus",
-        answers: ["Vrai (+)", "Faux (−)"],
-        correctIndex: 0
+        question: "Lequel de ces actes n'est pas considéré comme un délit en Allemagne ?",
+        type: "qcm",
+        answers: ["Conduire pieds nus", "Ouvrir le courrier de son conjoint", "Refuser de témoigner devant un tribunal", "S'évader de prison"],
+        correctIndex: 3
       },
       {
         question: "En quelle année a eu lieu la première 24 Heures du Mans ?",
         type: "qcm",
         answers: ["1913", "1923", "1931", "1947"],
         correctIndex: 1
-      }
+      },
+      {
+        question: "Le Roi Lion (1994) a-t-il rapporté plus ou moins d'argent que La Reine des Neiges ?",
+        type: "plusminus",
+        answers: ["+", "-"],
+        correctIndex: 1
+      },
+      {
+        question: "Quel est le pays ayant le plus de fuseaux horaires ?",
+        type: "qcm",
+        answers: ["Russie", "USA", "France", "Royaume-Uni"],
+        correctIndex: 2
+      },
+      {
+        question: "AC/DC ont-ils vendu plus ou moins d'albums que Queen ?",
+        type: "plusminus",
+        answers: ["+", "-"],
+        correctIndex: 1
+      },
+      {
+        question: "Quelle est la capitale du Kazakhstan ?",
+        type: "qcm",
+        answers: ["Almaty", "Achgabat", "Bichkek", "Astana"],
+        correctIndex: 3
+      },
+      {
+        question: "Quelle planète possède le plus grand nombre de lunes connues ?",
+        type: "qcm",
+        answers: ["Jupiter", "Saturne", "Uranus", "Neptune"],
+        correctIndex: 1
+      },
+      {
+        question: "Quelle lettre n'apparaît dans aucun symbole du tableau périodique ?",
+        type: "qcm",
+        answers: ["J", "Q", "W", "X"],
+        correctIndex: 0
+      },
+      {
+        question: "Quelle couleur attire le moins les moustiques ?",
+        type: "qcm",
+        answers: ["Noire", "Blanche", "Rouge", "Verte"],
+        correctIndex: 1
+      },
+      {
+        question: "Elton John a-t-il vendu plus ou moins d'albums que Madonna ?",
+        type: "plusminus",
+        answers: ["+", "-"],
+        correctIndex: 0
+      },
+      {
+        question: "Quel est le plus grand désert du monde ?",
+         type: "qcm",
+        answers: ["Sahara", "Gobi", "Antarctique", "Arabie"],
+        correctIndex: 2
+      },
+      {
+        question: "Quel roi de France était surnommé <<le Bien-Aimé>> ?",
+         type: "qcm",
+        answers: ["Louis XIII", "Louis XIV", "Louis XV", "Louis XVI"],
+        correctIndex: 2
+      },
+      {
+        question: "Quel est le seul État dont le drapeau national n'est pas un quadrilatère ?",
+         type: "qcm",
+        answers: ["Suisse", "Vatican", "Bhoutan", "Népal"],
+        correctIndex: 3
+      },
+      {
+        question: "Lequel de ces nombres est un nombre premier ?",
+         type: "qcm",
+        answers: ["221", "341", "391", "397"],
+        correctIndex: 1
+      },
+      
     ]
   },
 
